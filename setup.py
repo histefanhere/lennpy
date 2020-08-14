80af26a88c91c02da4c5a702a6fb9cfd3d20d770
Histefanhere <stefan.zdra@gmail.com>
2020-08-14 20:20:34 +1200

    Version update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lennpy",
-    version="0.0.1",
+    version="0.0.2",
     author="Stefan Zdravkovic",
     author_email="stefan.zdra@gmail.com",
     description="Generate Lenny Faces",
