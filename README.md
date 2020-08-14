ec92e9d6fba33bb795c6e7cb8c04c6fce9d31598
Histefanhere <stefan.zdra@gmail.com>
2020-08-14 20:58:39 +1200

    README and setup changes

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # lennpy
+
+Python Package for accessing [Lenny faces](https://knowyourmeme.com/memes/%CD%A1-%CD%9C%CA%96-%CD%A1-lenny-face)
+
+One day I woke up and decided that I needed lenny faces in my projects, but I was dissapointed when I found the lack of categorized and easily accessable lenny faces in Python. So I made this.
+
+## Installation
+
+```
+python3 -m pip install lennpy
+```
+
+## Usage
+
+If you just want the standard lenny faces, lennpy provides `lennpy.lenny` which holds the popular lenny face, among others:
+
+```py
+>>> import lennpy as le
+>>> le.lenny
+'( ͡° ͜ʖ ͡°)'
+>>> le.lenny.table_flip
+'(╯°□°)╯︵ ┻━┻'
+```
+
+But lennpy also organizes lenny faces by categories, _emotions,_ which from you can get a random face in various ways:
+
+```py
+>>> import lennpy as le
+>>> le.get_basic()
+'( ͡° ͜ʖ ͡°)'
+>>> le.emotions()
+['basic', 'table_flip', ...]
+>>> le.get('basic')
+'( ͡ᵔ ͜ʖ ͡ᵔ )'
+```
--- setup.py
+++ setup.py
@@ -1,21 +1,27 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding='utf-8') as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="lennpy",
-    version="0.0.2",
+    version="0.0.3",
     author="Stefan Zdravkovic",
     author_email="stefan.zdra@gmail.com",
     description="Generate Lenny Faces",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    keywords=['lenny', 'memes'],
     url="https://github.com/Histefanhere/lennpy",
     packages=setuptools.find_packages(),
+    license='GNU GPLv3',
     classifiers=[
+        "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: OS Independent",
+        "Operating System :: OS Independent"
     ]
 )
